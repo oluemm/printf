cd4afda08fa48bc5f3ce3255feabdb5c954e0440
Owoyomi <owoyomitaiwoadeyemi@gmail.com>
2022-08-05 13:30:41 +0100

    _printf

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _printf - function that replicates what printf does
+ * @format: a character string
+ *
+ * Return:  the number of characters printed
+ */
+int _printf(const char *format, ...)
+{}
