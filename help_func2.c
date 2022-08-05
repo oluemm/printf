6a8ef00c1a0f3c20837f67d5262eab1f1accd4a1
Owoyomi <owoyomitaiwoadeyemi@gmail.com>
2022-08-05 14:21:05 +0100

    Docs : convert - function that converts our int to hex, octal, or binary

--- help_func2.c
+++ help_func2.c
@@ -32,3 +32,27 @@ int _print_percent(va_list ap __attribute__((unused)))
 	_putchar('%');
 	return (1);
 }
+/**
+ * convert - function that converts our int to hex, octal, or binary
+ * @num: the number passed into the function
+ * @base: the base to convert to
+ *
+ * Return: the convertednumber of a certain base
+ */
+char *convert(unsigned int num, int base)
+{
+	const char Representation[] = "0123456789ABCDEF";
+	static char buffer[50];
+	char *ptr;
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	while (num != 0)
+	{
+		*--ptr = Representation[num % base];
+		num /= base;
+	}
+
+	return (ptr);
+}
