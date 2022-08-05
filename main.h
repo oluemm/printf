aba3a54ec793b7c6fb36453b27846759bdec41eb
fashemma007 <fasogbaemmanuel@gmail.com>
2022-08-05 13:51:29 +0100

    helper functions

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
+int _print_string(va_list ap);
+int _putchar(char c);
+int _print_char(va_list ap);
 
 #endif /* _MAIN_H_ */
