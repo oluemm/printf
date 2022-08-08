b813a9d017e2b54f6b53c38d993e52d31facd98b
fashemma007 <fasogbaemmanuel@gmail.com>
2022-08-08 14:38:27 +0100

    print_type: updates

--- _printf.c
+++ _printf.c
@@ -7,4 +7,26 @@
  * Return:  the number of characters printed
  */
 int _printf(const char *format, ...)
-{}
+{
+	print_type argument[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"%", _print_percent},
+		{"d", _print_int},
+		{"i", _print_int},
+		{"r", _print_reverse},
+		{"R", _print_rot13},
+		{"b", _print_binary},
+		{"u", _print_unsigned},
+		{"o", _print_octal},
+		{"x", _print_hex_l},
+		{"X", _print_hex_u},
+		{NULL, NULL}};
+	va_list ap;
+	int count = 0;
+
+	va_start(ap, format);
+	count = get_func(format, argument, ap);
+	va_end(ap);
+	return (count);
+}
