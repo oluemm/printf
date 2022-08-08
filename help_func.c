c1742e6519cb278b67316a65b09b8b2ed1110405
Owoyomi <owoyomitaiwoadeyemi@gmail.com>
2022-08-08 14:07:51 +0100

    function that prints an octal number

--- help_func.c
+++ help_func.c
@@ -80,3 +80,27 @@ int _print_unsigned(va_list ap)
 	}
 	return (count);
 }
+
+/**
+ * _print_octal - Prints an octal number
+ * @ap: Action pointer
+ *
+ * Return: Number of digits
+ */
+
+int _print_octal(va_list ap)
+{
+	int i;
+	unsigned int n;
+	char *s;
+	int count = 0;
+
+	n = va_arg(ap, unsigned int);
+	s = convert(n, 8);
+
+	for (i = 0; s[i]; i++)
+	{
+		count += _putchar(s[i]);
+	}
+	return (count);
+}
